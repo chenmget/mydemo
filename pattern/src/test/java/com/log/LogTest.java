949232f880f76b86b644a3787c24803610117de5
chenmget <502298798@qq.com>
2019-05-17 16:30:54 +0800

    logback

---
+++ pattern/src/test/java/com/log/LogTest.java
@@ -0,0 +1,15 @@
+package com.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogTest {
+    private final static Logger logger = LoggerFactory.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+       logger.info("logbak成功啦");
+       logger.warn("logbak成功啦");
+       logger.error("logbak成功啦");
+
+    }
+}
